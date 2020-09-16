d4cc73aba55d3643963ad82443e698d015194130
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-16 15:19:58 +0200

    handle css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 //package of node.js - allows us to constract an absolute path
 const path = require("path");
+const autoprefixer = require("autoprefixer");
 
 module.export = {
   mode: "development",
@@ -13,9 +14,37 @@ module.export = {
   module: {
     rules: [
       {
-        // all files which end with js will affected -> babel
+        // setup js files - all files -> babel
         test: /\.js$/,
         loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        // setup css files - all files -> style, and css loader
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              // enable css models
+              modules: {
+                // name of classes
+                loadlIdentName: "[name]__[local]__[hash:base64:5]",
+              },
+            },
+          },
+          // auto prefix
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [autoprefixer()],
+              // additional add browserslist to package.json
+            },
+          },
+        ],
         exclude: /node_modules/,
       },
     ],
